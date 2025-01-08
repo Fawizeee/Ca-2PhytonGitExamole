3148da0ca91ccb20387bb7577caacd241afd1b3f
fawaz <bellofawaz5@gmail.com>
2025-01-08 14:31:53 +0100

    a manually added todo-list

---
+++ todo-list.py
@@ -0,0 +1,6 @@
+print("--my todo list for today--");
+print("1. go to school");
+print("2. attend classes");
+print("3. take a brealk and eat");
+print("4. attend 2-4 classes");
+print("5. go home.")
